5b170682253a4f6c5303f30c0f39d36f389395b4
Antoine Bertin <diaoulael@gmail.com>
2012-01-17 21:05:38 +0100

    Use an Application class

--- dobby/app.py
+++ dobby/app.py
@@ -16,50 +16,191 @@
 # You should have received a copy of the GNU General Public License
 # along with Dobby.  If not, see <http://www.gnu.org/licenses/>.
 
+from Queue import Queue
 from configobj import ConfigObj, flatten_errors
-from dobby import infos
-from dobby.app import Application
 from dobby.controller import Controller
-from dobby.db import Session
+from dobby.db import initDb, Session
 from dobby.recognizers.julius import Julius as JuliusRecognizer
 from dobby.speakers.speechdispatcher import SpeechDispatcher
 from dobby.triggers.clapper import Pattern, QuietPattern, NoisyPattern, Clapper
 from dobby.triggers.julius import Julius as JuliusTrigger
 from validate import Validator, VdtValueError, VdtTypeError
-import argparse
+import atexit
 import logging.handlers
 import os
+import signal
 import sys
+import time
 
 
 logger = logging.getLogger()
+appdir = os.path.abspath(os.path.dirname(__file__))
 
 
-def main():
-    parser = argparse.ArgumentParser(description=infos.__description__)
-    parser.add_argument('-d', '--daemon', action='store_true', help='run as daemon', default=False)
-    parser.add_argument('-p', '--pid-file', action='store', dest='pidfile', help='create pid file')
-    parser.add_argument('-c', '--config-file', action='store', dest='configfile', help='config file to use')
-    parser.add_argument('--list-devices', action='store_true', dest='list_devices', help='list available devices and exit')
-    parser.add_argument('--data-dir', action='store', dest='data_dir', help='data directory to store cache, config, logs and database', default='data')
-    group_verbosity = parser.add_mutually_exclusive_group()
-    group_verbosity.add_argument('-q', '--quiet', action='store_true', help='disable console output')
-    group_verbosity.add_argument('-v', '--verbose', action='store_true', help='verbose console output')
-    parser.add_argument('--version', action='version', version=infos.__version__)
-    args = parser.parse_args()
+class Application(object):
+    def __init__(self, datadir, configfile=None, pidfile=None, quiet=False, verbose=False, daemon=False):
+        self.datadir = os.path.abspath(datadir)
+        if not os.path.exists(self.datadir):
+            os.makedirs(self.datadir)
+        self.pidfile = pidfile or os.path.join(self.datadir, 'dobby.pid')
+        self.configfile = configfile or os.path.join(self.datadir, 'config.ini')
+        self.quiet = quiet
+        self.verbose = verbose
+        self.daemon = daemon
+
+    def daemonize(self):
+        """Do the UNIX double-fork magic, see Stevens' "Advanced
+        Programming in the UNIX Environment" for details (ISBN 0201563177)
+        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+
+        """
+        try:
+            pid = os.fork()
+            if pid > 0:  # exit first parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write('Fork #1 failed: %d (%s)\n' % (e.errno, e.strerror))
+            sys.exit(1)
+        
+        # decouple from parent environment
+        os.chdir('/')
+        os.setsid()
+        os.umask(0)
+        
+        # do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:  # exit from second parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write('Fork #2 failed: %d (%s)\n' % (e.errno, e.strerror))
+            sys.exit(1)
+        
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file('/dev/null', 'r')
+        so = file('/dev/null', 'a+')
+        se = file('/dev/null', 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+        
+        # write pidfile
+        atexit.register(self.delpid)
+        file(self.pidfile, 'w+').write('%s\n' % str(os.getpid()))
     
-    if args.list_devices:
-        import pyaudio
-        pa = pyaudio.PyAudio()
-        for i in range(pa.get_device_count()):
-            device_infos = pa.get_device_info_by_index(i)
-            print device_infos['name'] + "\t" + str(device_infos['index'])
-        sys.exit(0)
+    def delpid(self):
+        os.remove(self.pidfile)
+    
+    def start(self):
+        """Start the application/daemon"""
+        # In case we run the application not as daemon, write pid and run
+        if not self.daemon:
+            file(self.pidfile, 'w+').write('%s\n' % str(os.getpid()))
+            self.run()
+            return
 
-    # Run the application
-    app = Application(os.path.abspath(args.data_dir), configfile=args.configfile, pidfile=args.pidfile, quiet=args.quiet, verbose=args.verbose, daemon=args.daemon)
-    app.start()
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            pf = file(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+        
+        if pid:
+            sys.stderr.write('pidfile %s already exist. Daemon already running?\n' % self.pidfile)
+            sys.exit(1)
+        
+        # Start the daemon
+        self.daemonize()
+        self.run()
+    
+    def stop(self):
+        """Stop the daemon"""
+        # Get the pid from the pidfile
+        try:
+            pf = file(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+        
+        if not pid:
+            sys.stderr.write('pidfile %s does not exist. Daemon not running?\n' % self.pidfile)
+            return # not an error in a restart
+        
+        # Try killing the daemon process       
+        try:
+            while 1:
+                os.kill(pid, signal.SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find('No such process') > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
+    
+    def restart(self):
+        """Restart the daemon"""
+        self.stop()
+        self.start()
 
+    def run(self):
+        # Init config
+        config = initConfig(self.configfile)
+        
+        # Init logging
+        initLogging(self.quiet, self.verbose, os.path.join(self.datadir, 'logs'))
+        
+        # Init db
+        initDb(os.path.join(self.datadir, 'dobby.db'))
+        
+        # Init recognizer
+        recognizer = initRecognizer(config['Recognizer'])
+    
+        # Init triggers
+        event_queue = Queue()
+        triggers = initTriggers(event_queue, recognizer, config['Trigger'])
+    
+        # Init speaker
+        tts_queue = Queue()
+        speaker = initSpeaker(tts_queue, config['Speaker'])
+    
+        # Start controller
+        controller = initController(event_queue, tts_queue, recognizer, config['General'])
+        
+        # Welcome message
+        if config['General']['welcome_message']:
+            tts_queue.put(config['General']['welcome_message'])
+
+        # Handle termination
+        def handler(*args):
+            logger.info(u'Stop signal caught')
+            if config['General']['bye_message']:
+                tts_queue.put(config['General']['bye_message'])
+            controller.stop()
+            controller.join()
+            for trigger in triggers:
+                trigger.stop()
+                trigger.join()
+            recognizer.stop()
+            recognizer.join()
+            speaker.stop()
+            speaker.join()
+            config.write()
+            exit(0)
+
+        # Plug signals
+        signal.signal(signal.SIGINT, handler)
+        signal.signal(signal.SIGTERM, handler)
+        
+        # Wait until it's time to exit
+        signal.pause()
 
 def initTriggers(event_queue, recognizer, config):
     """Initialize all triggers as defined in the config
@@ -176,7 +317,7 @@ def initConfig(path='config.ini'):
                 raise VdtValueError(v)
         return value
 
-    config = ConfigObj(path, configspec='config.spec', encoding='utf-8')
+    config = ConfigObj(path, configspec=os.path.join(appdir, 'config.spec'), encoding='utf-8')
     vtor = Validator({'option_list': is_option_list})
     results = config.validate(vtor, copy=True)
     if results != True:
@@ -187,7 +328,3 @@ def initConfig(path='config.ini'):
                 sys.stderr.write('The following section was missing: %s\n' % ', '.join(section_list))
         sys.exit(1)
     return config
-
-
-if __name__ == '__main__':
-    main()
